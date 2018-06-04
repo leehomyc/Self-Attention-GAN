c1511ee9a8d2cbffd4435b6c739e535391121d1d
Harry Yang <harryyang.hk@gmail.com>
2018-06-04 12:48:41 -0700

    before correcting the mistakes

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 CUDA_VISIBLE_DEVICES=1 python main.py --batch_size 6 --imsize 64 --dataset celeb --adv_loss hinge --version sagan_1
 
-CUDA_VISIBLE_DEVICES=1 python main.py --batch_size 6 --imsize 64 --dataset celeb --adv_loss hinge --version sagan_1 --use_inception True --use_tensorboard True --model_save_step 2
+CUDA_VISIBLE_DEVICES=1 python main.py --batch_size 6 --imsize 64 --dataset celeb --adv_loss hinge --version sagan_1 --use_inception True --use_tensorboard True --model_save_step 2 --pretrained_model 405192
